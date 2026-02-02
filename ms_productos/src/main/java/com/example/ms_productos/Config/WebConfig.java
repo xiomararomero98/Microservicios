1dc8b1ef971555f8e1dd4c8725c7d9a0132658f2
xiomararomero98 <xio.romero@duocuc.cl>
2026-02-02 00:56:04 -0300

    a

--- ms_productos/src/main/java/com/example/ms_productos/Config/WebConfig.java
+++ ms_productos/src/main/java/com/example/ms_productos/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
