5e88696b610c8225e9f60b7d78a03782aca78a29
xiomararomero98 <xio.romero@duocuc.cl>
2026-02-03 01:14:15 -0300

    creacion ms-ventas

---
+++ ms-ventas/src/main/java/com/example/ms_ventas/MsVentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVentasApplication.class, args);
+	}
+
+}
